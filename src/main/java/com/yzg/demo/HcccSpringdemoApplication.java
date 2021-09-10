b30b36ab7e6cfed63081e02e5b2d74165d439400
yezonggang <717818895@qq.com>
2021-09-10 15:04:04 +0800

    Initial commit

---
+++ src/main/java/com/yzg/demo/HcccSpringdemoApplication.java
@@ -0,0 +1,15 @@
+package com.yzg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcccSpringdemoApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("hello world!");
+		SpringApplication.run(HcccSpringdemoApplication.class, args);
+	}
+
+}
