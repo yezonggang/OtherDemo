dedb50d3bb23cc6f82d40281eee34349f4b0ce40
yezonggang <717818895@qq.com>
2021-09-13 09:25:13 +0800

    集成swagger-ui

---
+++ src/main/java/com/yzg/demo/base/JavaTestBase.java
@@ -0,0 +1,7 @@
+package com.yzg.demo.base;
+
+public class JavaTestBase {
+    public static void main(String[] args) {
+        System.out.println("helo");
+    }
+}
--- src/main/java/com/yzg/demo/controller/TestControllerOne.java
+++ src/main/java/com/yzg/demo/controller/TestControllerOne.java
@@ -3,6 +3,8 @@ package com.yzg.demo.controller;
 
 import com.yzg.demo.model.Student;
 import com.yzg.demo.service.TeacherServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.yzg.demo.service.TeacherServiceImpl;
@@ -10,12 +12,15 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 @RestController
+@Api("测试swagger的api")
 @RequestMapping("/One")
 public class TestControllerOne {
     @Autowired
     TeacherServiceImpl teacherimpl;
 
     @GetMapping("/getStudent")
+    @ApiOperation(value = "查询学生", notes = "xzxxxx")
+
     public List<Student> getStudent() throws ExecutionException, InterruptedException {
         return teacherimpl.getStudents().get();
     }
