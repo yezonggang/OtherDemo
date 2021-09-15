8a35c7490221c7eff815c6d7f8db875b032f41a8
yezonggang <717818895@qq.com>
2021-09-15 14:31:43 +0800

    微调

--- src/main/java/com/yzg/demo/base/JavaTestBase.java
+++ src/main/java/com/yzg/demo/base/JavaTestBase.java
@@ -1,7 +1,42 @@
 package com.yzg.demo.base;
 
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class JavaTestBase {
     public static void main(String[] args) {
-        System.out.println("helo");
+        System.out.println("start");
+
+        // 测试
+        int a=1;
+        Integer b=1;
+        System.out.println(a==b);
+        String c="tests";
+        System.out.println(c.equals("tests"));
+        System.out.println(c.contains("e"));
+        System.out.println(c.charAt(1));
+        System.out.println(c.compareTo("testssssssssss"));
+        System.out.println(c.concat("xxxx"));
+        System.out.println(c.substring(1,2));
+        System.out.println(c.endsWith("s"));
+        System.out.println(" sf  df ".trim());
+        System.out.println(c.length());
+        String[] d = c.split("s");
+        System.out.println(c.indexOf("s"));
+
+        List<String> stringList=new ArrayList<String>();
+        System.out.println(Optional.of(stringList).isPresent());
+
     }
+
+
+
+
+
+
+
+
 }
