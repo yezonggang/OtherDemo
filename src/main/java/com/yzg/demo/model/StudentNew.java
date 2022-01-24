44d686c2ef2bd2c15eb12dfc3524402ea0ab377a
yezonggang <717818895@qq.com>
2022-01-24 14:42:16 +0800

    集成mybatis-plus，修复已知bug

--- src/main/java/com/yzg/demo/model/StudentNew.java
+++ src/main/java/com/yzg/demo/model/StudentNew.java
@@ -1,5 +1,6 @@
 package com.yzg.demo.model;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -9,6 +10,8 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @Data
+// 如果数据库中的表名称和pojo类名称不对应，使用注解修改
+@TableName("STUDENT")
 public class StudentNew {
     public int ID;
     public String NAME;
